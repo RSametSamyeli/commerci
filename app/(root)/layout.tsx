a20d510d9b67acd22ed944d4a2f2fa9f6f04c5be
Samet Samyeli <ssamyeli@nuevo.com.tr>
2024-02-18 15:26:27 +0300

    Next.js routing and folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Layout = ({children}: {children:React.ReactNode}) => {
+  return (
+    <main className='root'>
+        <div className="root">
+            <div className="root-container">
+                <div className="wrapper">
+                 {children}
+                </div>
+            </div>
+        </div>  
+    </main>
+  )
+}
+
+export default Layout
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+// localhost:3000/transformations
+// localhost:3000/transformations[id] == next.js dynamic route
