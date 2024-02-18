e485cc8b23979be8a03ce2676ca4b0a7fee753cf
Samet Samyeli <ssamyeli@nuevo.com.tr>
2024-02-18 23:21:28 +0300

    Sign in and sign out feature added via clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({ 
@@ -20,8 +22,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
