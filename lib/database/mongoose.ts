0f90b00f0799b583cf3105e3327486b9ed7b432e
Samet Samyeli <ssamyeli@nuevo.com.tr>
2024-02-20 10:24:44 +0300

    Fix word/typo in mongoose.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -2,32 +2,31 @@ import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
-interface MongooseConnetion {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnetion = {global as any}.mongoose
+let cached: MongooseConnection = (global as any).mongoose
 
 if(!cached) {
-    cached = (global as any).mongoose = { 
-        conn: null, promise: null 
-    }
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
 export const connectToDatabase = async () => {
-    if(cached.conn) {
-        return cached.conn;
-    }
+  if(cached.conn) return cached.conn;
 
-    if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-    cached.promise = 
-        cached.promise || mongoose.connect(MONGODB_URL, {
-            dbName: 'Commerci', bufferCommands: false
-        })
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'Commerci', bufferCommands: false 
+    })
 
-    cached.conn = await cached.promise;
+  cached.conn = await cached.promise;
 
-    return cached.conn;
+  return cached.conn;
 }
