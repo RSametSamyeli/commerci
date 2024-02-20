9f05967a88fef0b5b96719525ccd74419fa85a83
Samet Samyeli <ssamyeli@nuevo.com.tr>
2024-02-21 00:26:26 +0300

    implement clerk webhooks

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -13,9 +13,9 @@ const Sidebar = () => {
   return (
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
-        <Link href="/" className="sidebar-logo">
+        {/* <Link href="/" className="sidebar-logo">
           <Image src="/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
-        </Link>
+        </Link> */}
 
         <nav className="sidebar-nav">
           <SignedIn>
