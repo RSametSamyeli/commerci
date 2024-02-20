22319a7cd8586495ac43d5f1312403dd4f72bfeb
Samet Samyeli <ssamyeli@nuevo.com.tr>
2024-02-20 10:30:07 +0300

    Fix something for deploy

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -73,3 +73,21 @@ export async function deleteUser(clerkId: string) {
   }
 }
 
+// USE CREDITS
+export async function updateCredits(userId: string, creditFee: number) {
+  try {
+    await connectToDatabase();
+
+    const updatedUserCredits = await User.findOneAndUpdate(
+      { _id: userId },
+      { $inc: { creditBalance: creditFee }},
+      { new: true }
+    )
+
+    if(!updatedUserCredits) throw new Error("User credits update failed");
+
+    return JSON.parse(JSON.stringify(updatedUserCredits));
+  } catch (error) {
+    handleError(error);
+  }
+}
