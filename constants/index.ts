00cda0408711ac62e377609ce60d01339416216f
Samet Samyeli <ssamyeli@nuevo.com.tr>
2024-02-21 07:34:59 +0300

    Refactor page components and add transformation types

--- constants/index.ts
+++ constants/index.ts
@@ -40,3 +40,45 @@ export const navLinks = [
       icon: "/assets/icons/bag.svg",
     },
   ];
+  export const transformationTypes = {
+    restore: {
+      type: "restore",
+      title: "Restore Image",
+      subTitle: "Refine images by removing noise and imperfections",
+      config: { restore: true },
+      icon: "image.svg",
+    },
+    removeBackground: {
+      type: "removeBackground",
+      title: "Background Remove",
+      subTitle: "Removes the background of the image using AI",
+      config: { removeBackground: true },
+      icon: "camera.svg",
+    },
+    fill: {
+      type: "fill",
+      title: "Generative Fill",
+      subTitle: "Enhance an image's dimensions using AI outpainting",
+      config: { fillBackground: true },
+      icon: "stars.svg",
+    },
+    remove: {
+      type: "remove",
+      title: "Object Remove",
+      subTitle: "Identify and eliminate objects from images",
+      config: {
+        remove: { prompt: "", removeShadow: true, multiple: true },
+      },
+      icon: "scan.svg",
+    },
+    recolor: {
+      type: "recolor",
+      title: "Object Recolor",
+      subTitle: "Identify and recolor objects from the image",
+      config: {
+        recolor: { prompt: "", to: "", multiple: true },
+      },
+      icon: "filter.svg",
+    },
+  };
+  
