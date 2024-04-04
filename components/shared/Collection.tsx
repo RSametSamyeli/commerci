193dc6505b8c5fc1af0af46ec68e4c6259ba6a07
Samet Samyeli <ssamyeli@nuevo.com.tr>
2024-04-04 14:34:53 +0300

    Refactor Collection Card styling

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -99,20 +99,19 @@ export const Card = ({ image }: { image: IImage }) => {
     <li>
       <Link href={`/transformations/${image._id}`} className="collection-card">
         <CardContainer className="inter-var">
-          <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+          <CardBody className="relative group/card hover:shadow-2xl hover:shadow-emerald-500/[0.1] bg-transparent border-white/[0.2] w-auto h-auto rounded-xl p-6 border  ">
             <CardItem
               translateZ="50"
-              className="text-xl font-bold text-neutral-600 dark:text-white"
+              className="text-xl font-bold text-white"
             >
               {image.title}
             </CardItem>
             <CardItem
               as="p"
               translateZ="60"
-              className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+              className="text-sm max-w-sm mt-2 text-neutral-300"
             >
-              {/* You can customize this text according to your needs */}
-              Hover over this card to unleash the power of CSS perspective
+              Go to the visual artwork <strong>{image.title}</strong>. AI-powered image generator.
             </CardItem>
             <CardItem translateZ="100" className="w-full mt-4">
               <CldImage
